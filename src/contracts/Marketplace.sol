228d37c715bf34b550e691f92c2f18e1976c4df5
MOULIDHARAN428 <moulidharan428@gmail.com>
2022-07-11 23:55:31 +0530

    ccontract part finished

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -10,10 +10,11 @@ contract Marketplace{
         uint id;
         string name;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
-    } 
-    event ProductCreated(uint id, string name, uint price, address owner, bool purchased);
+    }
+    event ProductCreated(uint id, string name, uint price, address payable owner, bool purchased);
+    event ProductPurchasesd(uint id, string name, uint price, address payable owner, bool purchased);
 
     constructor() {
         name="Mouli";
@@ -23,8 +24,21 @@ contract Marketplace{
         require(bytes(_name).length > 0);
         require(_price > 0);
         productCount++;
-        products[productCount]=Product(productCount,_name,_price,msg.sender,false);
+        products[productCount]=Product(productCount,_name,_price,payable(msg.sender),false);
         emit ProductCreated(products[productCount].id, products[productCount].name, products[productCount].price, products[productCount].owner, products[productCount].purchased);
     }
 
+    function purchaseProduct(uint _id) public payable{
+        Product memory _product = products[_id];
+        address payable _seller = _product.owner;
+        require(_product.id>0 && _product.id<=productCount);
+        require(msg.value>=_product.price);
+        require(_product.purchased==false);
+        require(_seller!=msg.sender);
+        _product.owner=payable(msg.sender);
+        _product.purchased=true;
+        products[_id]=_product;
+        payable(address(_seller)).transfer(msg.value);
+        emit ProductPurchasesd(productCount, _product.name, _product.price, payable(msg.sender), true);
+    }
 }
